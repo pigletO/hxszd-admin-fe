cc18a2183d738d075a9521b0ca0993110459cd1b
dujingsheng <dujingsheng@4zlink.com>
2020-03-11 19:03:49 +0800

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,35 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import BlogLogin from '@/components/BlogLogin.vue'
+import BlogIndex from '@/components/BlogIndex.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    // {
+    //   path: '/',
+    //   name: 'HelloWorld',
+    //   component: HelloWorld
+    // },
+    {
+      path: '/',
+      redirect: '/login'
+    },
+    {
+      path: '/index',
+      name: 'BlogIndex',
+      component: BlogIndex
+    },
+    {
+      path: '/manage',
+      redirect: '/login'
+    },
+    {
+      path: '/login',
+      name: 'BlogLogin',
+      component: BlogLogin
+    }
+  ]
+})
